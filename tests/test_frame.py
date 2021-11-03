c36b5f0c295f8bde5ed952dc0b38a14deccccfc0
chbndrhnns <chbndrhnns@users.noreply.github.com>
2021-11-03 17:48:02 +0100

    refactor: Rename throws to attempts

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -2,10 +2,10 @@ import pytest
 
 INITIAL_PIN_COUNT = 5
 
-THROWS_PER_FRAME = 3
+ATTEMPTS_PER_FRAME = 3
 
 
-class NoThrowsLeft(Exception):
+class NoAttemptsLeft(Exception):
     ...
 
 
@@ -16,14 +16,14 @@ class NoPinsLeft(Exception):
 class Game:
     def __init__(self):
         self._pins_left = INITIAL_PIN_COUNT
-        self._throws_left = THROWS_PER_FRAME
+        self._attempts_left = ATTEMPTS_PER_FRAME
 
     def throw(self, knocked_down_count: int):
-        if not self._throws_left:
-            raise NoThrowsLeft()
+        if not self._attempts_left:
+            raise NoAttemptsLeft()
         if not self._pins_left:
             raise NoPinsLeft()
-        self._throws_left -= 1
+        self._attempts_left -= 1
         self._pins_left -= knocked_down_count
 
 
@@ -46,5 +46,5 @@ def test_cannot_throw_if_throws_used_up(game):
     game.throw(0)
     game.throw(0)
     game.throw(0)
-    with pytest.raises(NoThrowsLeft):
+    with pytest.raises(NoAttemptsLeft):
         game.throw(0)
